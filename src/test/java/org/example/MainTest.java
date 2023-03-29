a517147971da7139e6b8820d673affa83061df04
Gustavo Soto <gsoto998@gmail.com>
2023-03-29 10:12:28 -0300

    Creacion de clase para testear los dos métodos del código.

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void testisPrime() {
+
+    }
+
+    @Test
+    public void testCopyVectors() {
+
+    }
+}
